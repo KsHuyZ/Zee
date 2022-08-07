90de8df65451c0e47c3edaaaf09953a4071acbed
KsHuyZ <92804746+KsHuyZ@users.noreply.github.com>
2022-08-07 17:58:09 +0700

    update

--- src/Routers.js
+++ src/Routers.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 import App from "./App";
 function Routers() {
   return (
-    <Router basename="/">
-      <Route exact path="/">
-        <App />
-      </Route>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+      </Routes>
     </Router>
   );
 }
